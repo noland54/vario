26c0297524d0fbc5e74ab8aaa0ffba5fd3f1ae09
noland54 <nolywonie@gmail.com>
2025-03-18 18:34:09 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html amp lang="en-ID">
+<!-- LANDING PAGE DIREKSITOTO -->
+<head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>DIREKSITOTO : Bandar Platform Situs Dana Toto 4D Terbaik 2025</title>
+   <meta name="description" content="DIREKSITOTO adalah bandar togel online terbaik yang menghadirkan platform terpercaya untuk permainan Toto 4D dengan sistem transaksi Dana yang aman dan cepat. Sebagai situs togel terbaik tahun 2025, DIREKSITOTO menawarkan pengalaman bermain yang nyaman dengan pasaran resmi, diskon menarik, dan hadiah kemenangan besar." />
+   <meta name="keywords" content="DIREKSITOTO"/>
+   <meta name="robots" content="index,follow" />
+   <link href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhNSuAuXgNqkYOqOu-NJcDl9AeainE2mQTcaenDaD_mhuf7NKixteLP4x_ww4qFhP5NtWsNEDu-IH6UbYiNi992e460ZhjU4P_ug2pCYvzfNa3DWJVkJzUVKSpigjegje4ggWxRFrrLLc42CO3owDQfmJcDyGmrCLpTFQrpzpazSZvDlzPkWyjbEpp0y5c/s225/download.jfif" rel="shortcut icon" type="image/x-icon" />
+   <link rel="canonical" href="#" />
+   <meta property="og:url" content="#" />
+   <meta property="og:site_name" content="DIREKSITOTO" />
+   <meta property="og:image:alt" content="DIREKSITOTO" />
+   <meta property="og:image" content="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjOiSQW0_uvowQGIja1Fg4EZRTqavl9TckxgzzZE-pPrioFAnSiMy_6HMNBIMHSYqe4XXiOpnL0J1AToUnccMtSAiy_uG3ENhGzY0Mdpyd1bcCpVXHXStqZvpoIwSWQE2B8H76m-DyG3opJqwIzAFtk8u_RHvKz9w_lv-s_DQ9lZ-rFZW3qkPqw20dF_zE/s663/photo_2025-03-01_00-43-07.jpg" />
+    <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js" />
+   <script async src="https://cdn.ampproject.org/v0.js"></script>
+   <style amp-boilerplate>
+      body {
+         -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+         -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+         -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+         animation: -amp-start 8s steps(1, end) 0s 1 normal both
+      }
+
+      @-webkit-keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+
+      @-moz-keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+
+      @-ms-keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+
+      @-o-keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+
+      @keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+   </style>
+   <noscript>
+      <style amp-boilerplate>
+         body {
+            -webkit-animation: none;
+            -moz-animation: none;
+            -ms-animation: none;
+            animation: none
+         }
+      </style>
+   </noscript>
+
+   <style amp-custom>
+      * {
+         box-sizing: border-box;
+      }
+
+      body {
+         font-family: monospace;
+         position: relative;
+         background: #000000;
+         background-image: radial-gradient(#000000 5%, transparent 50%);
+         background-size: 5px 5px;
+         background-attachment: fixed;
+      }
+
+      .container {
+         max-width: 400px;
+         height: 100vh;
+         margin: 0 auto;
+         padding: 15px;
+         display: flex;
+         flex-direction: column;
+         justify-content: center;
+         overflow: hidden;
+      }
+
+      .welcome {
+         max-width: 350px;
+         margin: 0 auto;
+      }
+
+      .welcome h3 {
+         font-size: 12px;
+         color: #fff;
+         overflow: hidden;
+         border-right: .15em solid #fbf4a0;
+         /* The typwriter cursor */
+         white-space: nowrap;
+         letter-spacing: .09em;
+         animation: typing 3.5s steps(40, end), blink-caret .75s step-end infinite;
+      }
+
+      @keyframes typing {
+         from {
+            width: 0
+         }
+
+         to {
+            width: 100%
+         }
+      }
+
+      @keyframes blink-caret {
+
+         from,
+         to {
+            border-color: transparent
+         }
+
+         50% {
+            border-color: #fbf4a0;
+         }
+      }
+
+      .banner {
+         border-radius: 10px;
+      }
+
+      .btn-login {
+         padding: 10px;
+         color: #ffffff;
+         font-size: 20px;
+         font-weight: bold;
+         text-decoration: none;
+         text-align: center;
+         background: linear-gradient(to bottom, #000000 100%, rgb(0, 255, 221) 0%);
+         border: 3px solid rgb(255, 255, 0);
+         margin-top: 10px;
+         border-radius: 10px;
+         position: relative;
+         z-index: 2;
+      }
+
+      .btn-daftar {
+         padding: 10px;
+         color: #fff;
+         font-size: 20px;
+         font-weight: bold;
+         text-decoration: none;
+         text-align: center;
+         background: linear-gradient(to bottom, #000000 100%, rgb(255, 165, 0) 0%);
+         border: 3px solid rgb(255, 255, 0);
+         margin-top: 10px;
+         border-radius: 10px;
+         position: relative;
+         z-index: 2;
+      }
+
+      .copyright {
+         text-align: center;
+         color: #ffffff96;
+      }
+
+      .nagaterbang {
+         position: fixed;
+         z-index: 1;
+         left: 0;
+         right: 0;
+         top: 0;
+         bottom: 0;
+         margin: auto;
+      }
+   </style>
+</head>
+<body>
+   <div class="container">
+      <header>
+         <center><a href="#"><amp-img id="image"
+                  class="animated tada infinite slower" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiyKGmZ_gJvqP14xVy5NuJQiVLxFdp1bm19Jlxj18vi83JgoIHRR0fJU40ENOZrMEHeJ85EutwEi2UJ_FAHK4aDvTVkIMyD-FoxEooBcEL9QkzwF4-vdtQs3irLlrZKP2KiqRtNxxw_Xaz8g41TAerHFqHo0lY8IogpX5DBjh5pdAuSQAKfcgDYOlKhcmk/s399/logoweb.png"
+                  alt="Sbobet" width="250" height="50" layout="intrinsic"></amp-img></a></center>
+      </header>
+      <div class="welcome">
+         <center><h3>Ketik Di Google : DIREKSITOTO</h3></center>
+      </div>
+
+      <amp-img
+         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjOiSQW0_uvowQGIja1Fg4EZRTqavl9TckxgzzZE-pPrioFAnSiMy_6HMNBIMHSYqe4XXiOpnL0J1AToUnccMtSAiy_uG3ENhGzY0Mdpyd1bcCpVXHXStqZvpoIwSWQE2B8H76m-DyG3opJqwIzAFtk8u_RHvKz9w_lv-s_DQ9lZ-rFZW3qkPqw20dF_zE/s663/photo_2025-03-01_00-43-07.jpg"
+         width="500" height="500" layout="intrinsic" class="banner"></amp-img>
+<br>
+
+      <a href="https://t.ly/Direksitotogacor" target="_blank" rel="noreferrer noopener"
+         role="button" class="btn-daftar">LOGIN DIREKSITOTO</a>
+      <a href="https://t.ly/Direksitotogacor" target="_blank" rel="noreferrer noopener"
+         role="button" class="btn-login">DAFTAR DIREKSITOTO</a>
+		
+	 	  
+      <p class="copyright">Copyright © 2025 • TEAM JALUR LANGIT
+	 </p>
+   </div>
+</body>
+</html>
